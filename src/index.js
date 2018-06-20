72ab0a6d804867edb267cc58a539d213134486e1
mnap <learn@10g.pl>
2018-06-20 19:58:37 +0200

    Add date format

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const formatDate = (timeInSeconds) => {
-    return `${timeInSeconds}s`;
-}
+    if (timeInSeconds === 0 || timeInSeconds == '') {
+        return '0s';
+    } else if (timeInSeconds < 60) {
+        return `${timeInSeconds}s`;
+    } else if (timeInSeconds < 3600) {
+        if (timeInSeconds % 60 === 0) {
+            return `${timeInSeconds / 60}m`;
+        } else {
+            return `${Math.floor(timeInSeconds / 60)}m ${timeInSeconds % 60}s`;
+        }
+    } else {
+        if (timeInSeconds % 3600 === 0) {
+            return `${timeInSeconds / 3600}h`;
+        } else if (timeInSeconds % 60 === 0) {
+            return `${Math.floor(timeInSeconds / 3600)}h ${timeInSeconds % 3600 / 60}m`;
+        } else {
+            return `${Math.floor(timeInSeconds / 3600)}h ${Math.floor(timeInSeconds % 3600 / 60)}m ${timeInSeconds % 3600 % 60}s`;
+        }
+    }
+};
 
 module.exports = formatDate;
