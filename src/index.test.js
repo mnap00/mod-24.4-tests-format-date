db9ed3fcebcd4cec07a211e6b25444f309c412c6
mnap <learn@10g.pl>
2018-06-20 20:22:33 +0200

    Fix whitespace

--- src/index.test.js
+++ src/index.test.js
@@ -5,17 +5,17 @@ describe('formatDate', () => {
     describe('seconds', () => {
         it('should return seconds', () => {
             const actual = formatDate(23);
-    
+
             const expected = '23s';
-    
+
             assert.equal(actual, expected);
         });
 
         it('should return 0 seconds if no value is passed', () => {
             const actual = formatDate();
-    
+
             const expected = '0s';
-    
+
             assert.equal(actual, expected);
         });
     });
